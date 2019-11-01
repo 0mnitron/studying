656ee7899f4f3dccefad68b32c9cce1d43bf896d
0mnitron <56760837+0mnitron@users.noreply.github.com>
2019-11-01 13:01:18 +0100

    Create BiljZad5.c

---
+++ C school/05-While/BiljZad5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+  int i,prost=0;
+  float br;
+  
+  printf("\nUpisi broj: ");
+  scanf("%f",&br);
+  
+  while(br==(int)br && br>0){
+    for(i=2; i<=br/2; i++){
+      if((int)br%i==0){
+        prost=1;
+        break;
+      }
+    }
+     
+    if(prost==0){
+      printf("\nBroj %.0f je prosti broj.",br);
+    }else{
+      printf("\nBroj %.0f nije prosti broj.",br);
+    }
+
+    prost=0;
+
+    printf("\nUpisi sljedeci broj: ");
+    scanf("%f",&br);
+  }
+  return 0;
+}
