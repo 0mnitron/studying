a312d20884c2d13c28de3717ed88ae102ae90d41
0mnitron <56760837+0mnitron@users.noreply.github.com>
2019-11-09 00:09:45 +0100

    Create lv11z5.c

---
+++ C school/07-Jednodimenzionalna polja/lv11z5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+  int a[5], b[5], c[5], i;
+
+  for(i=0; i<5; i++){
+    printf("\nUpisi broj u prvo polje: ");
+    scanf("%d",&a[i]);
+
+    printf("\nUpisi broj u drugo polje: ");
+    scanf("%d",&b[i]);
+
+    c[i] = a[i] + b[i];
+  }
+
+  printf("\nZbroj prvog i drugog polja: ");
+  for(i=0; i<5; i++){
+    printf("\n%d", c[i]);
+  }
+
+  return 0;
+}
