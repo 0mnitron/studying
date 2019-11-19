7cee5f88abd3ffbbe5f36346e6cf380b02942bb7
0mnitron <56760837+0mnitron@users.noreply.github.com>
2019-11-19 20:40:47 +0100

    Create BiljZad2.c

---
+++ C school/09-Ugnježdene petlje/BiljZad2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+  float br;
+  int i, prost=0, j;
+
+  for(i=0; i<5; i++){
+    do{
+      printf("\nUpisi prirodni broj: ");
+      scanf("%f", &br);
+    }while(br != (int)br || br <= 0);
+
+    for(j=2; j<=br/2; j++){
+      if((int)br%j == 0){
+        prost = 1;
+        break;
+      }
+    }
+
+    if(prost == 0){
+      printf("\nUpisani broj %.0f je prost broj", br);
+    }else{
+      printf("\nUpisani broj %.0f nije prost broj", br);
+    }
+    prost = 0;
+  }
+  
+  return 0;
+}
