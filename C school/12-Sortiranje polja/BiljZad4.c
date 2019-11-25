94e6ee7bd9a7ec445c32d50a33719abc4d00d410
0mnitron <56760837+0mnitron@users.noreply.github.com>
2019-11-25 19:17:41 +0100

    Update BiljZad4.c

--- C school/12-Sortiranje polja/BiljZad4.c
+++ C school/12-Sortiranje polja/BiljZad4.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
   int i, j;
-  float br[10], t;
+  float br[100], t;
 
-  for(i=0; i<10; i++){
+  for(i=0; i<100; i++){
     printf("\nUpisi realni broj: ");
     scanf("%f", &br[i]);
   }
 
-  for(i=0; i<10-1; i++){
-    for(j=i+1; j<10; j++){
+  for(i=0; i<100-1; i++){
+    for(j=i+1; j<100; j++){
       if(br[i]-(int)br[i] > br[j]-(int)br[j]){
         t = br[i];
         br[i] = br[j];
@@ -19,7 +19,7 @@ int main(){
   }
 
   printf("\nVrijednosti u polju po velicini decimalnog ostatka od najmanjeg prema najvecem: \n");
-  for(i=0; i<10; i++){
+  for(i=0; i<100; i++){
     printf("%f ", br[i]);
   }
 
