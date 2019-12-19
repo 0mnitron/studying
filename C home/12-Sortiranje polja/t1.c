2a68a5c31d7a6ea741f764397c77b49cb6b598ef
0mnitron <56760837+0mnitron@users.noreply.github.com>
2019-12-19 22:30:54 +0100

    Update t1.c

--- C home/12-Sortiranje polja/t1.c
+++ C home/12-Sortiranje polja/t1.c
@@ -3,41 +3,41 @@ int main(){
   int br[100], i, j, t;
 
   for(i=0; i<100; i++){
-    printf("\nUpisi %d. broj u polje: ",i+1);
+    printf("\nUpisi %d. broj: ",i+1);
     scanf("%d", &br[i]);
   }
 
-  for(i=0; i<100-1; i++){
-    if(i<33){
-      for(j=i+1; j<33; j++){
-        if(br[i] > br[j]){
-          t = br[i];
-          br[j] = br[i];
-          br[j] = t;
-        }
+  for(i=0; i<33-1; i++){
+    for(j=i+1; j<33; j++){
+      if(br[i] > br[j]){
+        t = br[i];
+        br[i] = br[j];
+        br[j] = t;
       }
-    }else if(i<66){
-      for(j=i+1; j<66; j++){
-        if(br[i] < br[j]){
-          t = br[i];
-          br[i] = br[j];
-          br[j] = t;
-        }
+    }
+  }
+  for(i=33; i<66-1; i++){
+    for(j=i+1; j<66; j++){
+      if(br[i] < br[j]){
+        t = br[i];
+        br[i] = br[j];
+        br[j] = t;
       }
-    }else{
-      for(j=i+1; j<100; j++){
-        if(br[i] > br[j]){
-          t = br[i];
-          br[i] = br[j];
-          br[j] = t;
-        }
+    }
+  }
+  for(i=66; i<100-1; i++){
+    for(j=i+1; j<100; j++){
+      if(br[i] > br[j]){
+        t = br[i];
+        br[i] = br[j];
+        br[j] = t;
       }
     }
   }
 
   printf("\nSortirani brojevi: \n");
   for(i=0; i<100; i++){
-    printf("%d ",br[i]);
+  printf("%d ",br[i]);
   }
   
   return 0;
