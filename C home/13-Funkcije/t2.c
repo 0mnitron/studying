1b14117d4746ac52a73ab0312a47e5de98a199e0
0mnitron <56760837+0mnitron@users.noreply.github.com>
2020-02-26 15:35:51 +0100

    Create t2.c

---
+++ C home/13-Funkcije/t2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void najveci(float x);
+int main(){
+  float br;
+  int i;
+
+  for(i=0; i<100; i++){
+    printf("\nUpisi %d. broj: ", i+1);
+    scanf("%f", &br);
+    najveci(br);
+  }
+  return 0;
+}
+
+void najveci(float x){
+  int tis, sto, des, max;
+
+  if(x != (int)x){
+    tis = (int)(x*1000)%10;
+   sto = (int)(x*100)%10;
+    des = (int)(x*10)%10;
+
+    max = tis;
+    if(sto > max){
+      max = sto;
+    }
+    if(des > max){
+      max = des;
+    }
+
+    printf("\nNajveca znamenka tisucnina, stotnina i desetnina ucitanog broja je znamenka %d", max);
+  }
+  return;
+}
